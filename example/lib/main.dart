10b572fce32cd322f7d321e44d2738ac276051bb
Fernando-H-Garcia <fernando.henrique.garcia@alumni.usp.br>
2023-06-14 11:03:15 -0300

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import './MainPage.dart';
+import 'Main_A300.dart';
+import 'Teste IA 2.dart';
+
+void main() => runApp(new ExampleApplication());
+
+class ExampleApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return MaterialApp(home: MainPage());
+    return MaterialApp(home:MyHomePage());
+  }
+}
